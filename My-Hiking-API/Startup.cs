2fd08682c93719c4d6d3c094a3fb53812d720a93
Mara1969 <mariyahsajjad@gmail.com>
2025-12-10 09:47:16 +0000

    added comments

--- My-Hiking-API/Startup.cs
+++ My-Hiking-API/Startup.cs
@@ -1,11 +1,13 @@
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
 using MyHikingAPI.Services;
+using Microsoft.Extensions.Logging;
 
-[assembly: FunctionsStartup(typeof(MyHikingAPI.Startup))]
+[assembly: FunctionsStartup(typeof(MyHikingAPI.Startup))] // Registers this class as the functions startup for DI config
 
 namespace MyHikingAPI;
 
+// Startup class where services for DI are registered 
 public class Startup : FunctionsStartup
 {
     public override void Configure(IFunctionsHostBuilder builder)
@@ -13,9 +15,9 @@ public class Startup : FunctionsStartup
          
         builder.Services.AddHttpClient();
 
-        builder.Services.AddSingleton<IMountainService, MountainService>();
+        builder.Services.AddSingleton<IMountainService, MountainService>(); // Registers domain service so it can be injected 
         
 
-        //builder.Services.AddSingleton<ILoggerProvider, MyLoggerProvider>();
+        //builder.Services.AddSingleton<ILoggerProvider, MyHikingAPI(); // Registers a custom logger 
     }
 }
