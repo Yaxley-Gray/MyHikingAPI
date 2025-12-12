6618357b25c6a0bd4bfb9dd402be45ce0e870f9c
Mara1969 <mariyahsajjad@gmail.com>
2025-12-12 16:57:11 +0000

    created services folder and renamed test file

---
+++ MyHikingAPI.Tests/Services/MountainServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using MyHikingAPI.Services;
+
+
+namespace MyHikingAPI.Tests
+{
+    public class MountainServiceTest
+    {
+        [Fact]
+        public void GetAllMountains_CheckIfReturnsPopulatedListOfMountains()
+        {
+            //Arrange 
+            MountainService mountainService = new MountainService(); 
+            
+            // Creates a new instance of mountainService using its default constructor
+            // This object will be used to call the service methods
+
+            //Act 
+            var mountains = mountainService.GetAllMountains(); 
+            
+            // Calls the GetAllMountains() method on the mountainService instance.
+            // The method returns a List<Mountain>, which is stored in the variable 'mountains'.
+            
+            //Assert 
+            mountains.Should().NotBeNullOrEmpty(); // checks if list is not null or empty 
+            mountains.Count.Should().Be(5); // Check if the list contains exactly 5 items 
+            mountains.Should().Contain(m => m.Name == "K2"); // checks if the list contains a mountain with name "K2"
+
+        }
+    
+        }
+    }
+
