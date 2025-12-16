fe5c420e0ccfca4d08119b2817963526efaf2218
Mara1969 <mariyahsajjad@gmail.com>
2025-12-16 11:11:09 +0000

    changed mountains table back to what it was with locations as text rather than GUID

--- DatabaseFiles/MyHikingAppDatabase/MountainsTable.sql
+++ DatabaseFiles/MyHikingAppDatabase/MountainsTable.sql
@@ -3,7 +3,5 @@ CREATE TABLE [dbo].[MountainsTable]
   [Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(),
   [Name] NVARCHAR(100) NOT NULL,
   [Height] INT NOT NULL,
-  [Location_Id] UNIQUEIDENTIFIER NOT NULL,
-  CONSTRAINT FK_Mountains_Locations FOREIGN KEY (Location_Id)
-    REFERENCES [dbo].[LocationsTable] ([Id])
+  [Location] NVARCHAR(100) NOT NULL, 
 );
