7f8ee32b3e4cea6572a095b82738645b9a514438
Mara1969 <mariyahsajjad@gmail.com>
2025-12-16 11:10:16 +0000

    adding normalised mountains table which joins locations table and mountains table

---
+++ DatabaseFiles/MyHikingAppDatabase/MountainsNormalised.sql
@@ -0,0 +1,11 @@
+CREATE TABLE [dbo].[MountainsNormalised]
+(
+  [Id] UNIQUEIDENTIFIER NOT NULL,
+  [Name] NVARCHAR(100) NOT NULL,
+  [Height] INT NOT NULL,
+  [LocationId] UNIQUEIDENTIFIER NOT NULL,
+  CONSTRAINT PK_MountainsNormalised PRIMARY KEY ([Id])
+  CONSTRAINT FK_MountainsNormalised_Locations 
+      FOREIGN KEY (LocationId)
+    REFERENCES LocationsTable([Id])
+);
