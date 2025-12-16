3d94887b029b940d23792d510a6c4abe8f8398b8
Mara1969 <mariyahsajjad@gmail.com>
2025-12-16 12:17:26 +0000

    refactoring

--- DatabaseFiles/MyHikingAppDatabase/MountainsNormalised.sql
+++ DatabaseFiles/MyHikingAppDatabase/MountainsNormalised.sql
@@ -4,7 +4,7 @@ CREATE TABLE [dbo].[MountainsNormalised]
   [Name] NVARCHAR(100) NOT NULL,
   [Height] INT NOT NULL,
   [LocationId] UNIQUEIDENTIFIER NOT NULL,
-  CONSTRAINT PK_MountainsNormalised PRIMARY KEY ([Id])
+  CONSTRAINT PK_MountainsNormalised PRIMARY KEY ([Id]),
   CONSTRAINT FK_MountainsNormalised_Locations 
       FOREIGN KEY (LocationId)
     REFERENCES LocationsTable([Id])
