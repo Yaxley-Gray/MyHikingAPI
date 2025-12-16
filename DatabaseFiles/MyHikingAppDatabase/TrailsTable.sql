8af1aef4db85b3f0cf9d13e45f55e43378fd8a80
Mara1969 <mariyahsajjad@gmail.com>
2025-12-16 17:04:21 +0000

    changing trails table to contain mountain name rather than mountain id

--- DatabaseFiles/MyHikingAppDatabase/TrailsTable.sql
+++ DatabaseFiles/MyHikingAppDatabase/TrailsTable.sql
@@ -2,10 +2,7 @@ CREATE TABLE [dbo].[TrailsTable]
 (
   [Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT NEWID(),
   [Name] NVARCHAR(100) NOT NULL,
-  [MountainId] UNIQUEIDENTIFIER NOT NULL,
+  [MountainName] NVARCHAR(100) NOT NULL,
   [Difficulty] NVARCHAR(100) NOT NULL,
   [Distance] DECIMAL(6,2) NOT NULL, -- this is in km 
-  CONSTRAINT FK_Trails_MountainsId
-      FOREIGN KEY ([MountainId])
-    REFERENCES MountainsNormalised([Id])
 );
